6112b50e939e09f9c968b7b90086276f321dc48d
Imranhossain45 <smimranhossain45@gmail.com>
2025-08-03 06:59:56 +0600

    Navbar active with reload

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,20 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { FaBars } from "react-icons/fa";
 import { IoMdClose } from "react-icons/io";
-import { Link } from "react-router";
+import { Link, useLocation } from "react-router-dom";
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const [activeLink, setActiveLink] = useState('/');
+  const location = useLocation();
+  useEffect(()=>{
+    setActiveLink(location.pathname || '/');
+  },[location.pathname]);
+
   const toggleMenu = () => {
     setIsOpen(!isOpen);
+  };
+  const handleLinkClick = (path) => {
+    setActiveLink(path);
   };
   return (
     <nav className="bg-green-800 text-white py-4 md:py-6 sticky top-0">
@@ -18,23 +27,98 @@ const Navbar = () => {
           </button>
         </div>
         <ul className="hidden md:flex space-x-4 md:space-x-6">
-          <li> <Link className="hover:text-green-300">Home</Link> </li>
-          <li> <Link to={"products"} className="hover:text-green-300">Products</Link> </li>
-          <li> <Link to={'blogs'} className="hover:text-green-300">Blogs</Link> </li>
-          <li> <Link to={'about'} className="hover:text-green-300">About</Link> </li>
-          <li> <Link to={'contact'} className="hover:text-green-300">Contact</Link> </li>
+          <li>
+            <Link
+              to={"/"}
+              className={`${activeLink === '/' ? 'text-green-300' : 'hover:text-green-300'}`}
+              onClick={() => handleLinkClick("/")}
+            >
+              Home
+            </Link>
+          </li>
+          <li>
+            {" "}
+            <Link
+              to={"/products"}
+              className={`${activeLink === '/products' ? 'text-green-300' : 'hover:text-green-300'}`}
+              onClick={()=>handleLinkClick('/products')}
+            >
+              Products
+            </Link>{" "}
+          </li>
+          <li>
+            {" "}
+            <Link
+              to={"/blogs"}
+              className={`${activeLink === '/blogs' ? 'text-green-300' : 'hover:text-green-300'}`}
+              onClick={()=>handleLinkClick('/blogs')}
+            >
+              Blogs
+            </Link>{" "}
+          </li>
+          <li>
+            {" "}
+            <Link
+              to={"/about"}
+              className={`${activeLink === '/about' ? 'text-green-300' : 'hover:text-green-300'}`}
+              onClick={()=>handleLinkClick('/about')}
+            >
+              About
+            </Link>{" "}
+          </li>
+          <li>
+            {" "}
+            <Link
+              to={"/contact"}
+              className={`${activeLink === '/contact' ? 'text-green-300' : 'hover:text-green-300'}`}
+              onClick={()=>handleLinkClick('/contact')}
+            >
+              Contact
+            </Link>{" "}
+          </li>
         </ul>
-        <button className="hidden md:block bg-green-600 px-4 py-2 rounded cursor-pointer hover:bg-green-500">Login</button>
+        <button className="hidden md:block bg-green-600 px-4 py-2 rounded cursor-pointer hover:bg-green-500">
+          Login
+        </button>
         {/* mobile menu collapse */}
-        <div className={`md:hidden w-full absolute bg-green-900 top-full left-0 ${isOpen ? 'block' : 'hidden' }`}>
+        <div
+          className={`md:hidden w-full absolute bg-green-900 top-full left-0 ${
+            isOpen ? "block" : "hidden"
+          }`}
+        >
           <ul className="flex flex-col items-center py-2">
-             <li> <Link className="hover:text-green-300">Home</Link> </li>
-              <li> <Link to={'products'} className="hover:text-green-300">Products</Link> </li>
-              <li> <Link to={'blogs'} className="hover:text-green-300">Blogs</Link> </li>
-              <li> <Link to={'about'} className="hover:text-green-300">About</Link> </li>
-              <li> <Link to={'contact'} className="hover:text-green-300">Contact</Link> </li>
             <li>
-              <button className="bg-green-600 px-4 py-2 rounded cursor-pointer hover:bg-green-500">Login</button>
+              {" "}
+              <Link className="hover:text-green-300">Home</Link>{" "}
+            </li>
+            <li>
+              {" "}
+              <Link to={"products"} className="hover:text-green-300">
+                Products
+              </Link>{" "}
+            </li>
+            <li>
+              {" "}
+              <Link to={"blogs"} className="hover:text-green-300">
+                Blogs
+              </Link>{" "}
+            </li>
+            <li>
+              {" "}
+              <Link to={"about"} className="hover:text-green-300">
+                About
+              </Link>{" "}
+            </li>
+            <li>
+              {" "}
+              <Link to={"contact"} className="hover:text-green-300">
+                Contact
+              </Link>{" "}
+            </li>
+            <li>
+              <button className="bg-green-600 px-4 py-2 rounded cursor-pointer hover:bg-green-500">
+                Login
+              </button>
             </li>
           </ul>
         </div>
